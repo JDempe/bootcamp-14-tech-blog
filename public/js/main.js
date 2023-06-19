9bd200830858a2b7fc4fc940cfe687af1215a684
JDempe <123279032+JDempe@users.noreply.github.com>
2023-06-19 19:20:38 -0400

    Finalized main styling

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,12 @@
 $(document).ready(function () {
+  // Enable tooltips everywhere
+  var tooltipTriggers = document.querySelectorAll('[data-toggle="tooltip"]');
+  for (var i = 0; i < tooltipTriggers.length; i++) {
+    new bootstrap.Tooltip(tooltipTriggers[i]);
+  }
+
   // Load the username and avatar to the navbar.  Only do this if the element is loaded
-  if ($("#nav-user-dropdown").length) {
+  if ($("#user-username").length) {
     // get the user's session data
     $.ajax({
       url: "/api/user/session/lookup",
@@ -11,14 +17,4 @@ $(document).ready(function () {
       }
     });
   }
-
-    //function to enable tooltip on navbar
-    document.addEventListener("DOMContentLoaded", function() {
-      console.log("DOM loaded and ready to go!");
-      var tooltipTriggers = document.querySelectorAll('[data-toggle="tooltip"]');
-      for (var i = 0; i < tooltipTriggers.length; i++) {
-          new bootstrap.Tooltip(tooltipTriggers[i]);
-      }
-   });
-   
 });

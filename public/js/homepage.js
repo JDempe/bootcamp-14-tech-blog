44c63502e3e1492af8183a676b7e0ca1a822e5af
JDempe <123279032+JDempe@users.noreply.github.com>
2023-06-20 16:16:42 -0400

    Updated homepage js to reflect new html element names

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   //  if any post is clicked on, expand the card to show the comments section
   $(".show-comment-btn").click(function () {
-    // Find the siblig comment-section and toggle the show/hidden
+    // Find the sibling comment-section and toggle the show/hidden
     $(this).siblings(".comment-section").slideToggle();
 
     // change the text to hide comments
@@ -12,8 +12,8 @@ $(document).ready(function () {
 
   $(".add-comment-btn").click(function () {
     // if the add comment btn is clicked, take the text from text area, add it to the db and then append it to the comment section
-    var commentText = $(this).siblings(".comment-text").val();
-    var commentTextArea = $(this).siblings(".comment-text");
+    var commentText = $(this).siblings(".comment-text-form").val();
+    var commentTextArea = $(this).siblings(".comment-text-form");
     var postId = $(this).closest(".post").data("postid");
     var commentSection = $(this).parents(".comment-section");
     var commentList = commentSection.children(".comment-list");
@@ -36,19 +36,12 @@ $(document).ready(function () {
         commentList.children(".no-comment-message").remove();
       }
 
-      // get request for the just created comment for the createdAt date because it gives a different format for some reason
-      $.ajax({
-        url: "/api/comments/" + data.id,
-        method: "GET",
-      }).then(function (data) {
-
-
       // append the comment to the comment list
       commentList.append(
-        `<div class="comment-date">${data.createdAt}</div>
-        <div class="comment card">
-          <div class="comment-text">${commentText}</div>
+        `<div class="comment card">
+          <div class="comment-text">"${commentText}"</div>
           <div class="comment-username">- ${data.username}</div>
+          <div class="comment-date">${data.createdAt}</div>
         </div>`
       );
 
@@ -58,6 +51,3 @@ $(document).ready(function () {
 
   });
   });
-
-
-});
